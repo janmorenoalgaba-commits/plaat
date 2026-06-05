de7089619f567cf23e86b562f3be20bcce12cc0f
janmorenoalgaba-commits <janmorenoalgaba@gmail.com>
2026-06-06 00:58:08 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -2,5 +2,5 @@
 // Pega aquí los datos de tu proyecto: Supabase → Settings → API
 // (La "anon key" es pública por diseño, es seguro tenerla aquí)
 
-export const SUPABASE_URL = 'https://enaickcisttilfkiclyv.supabase.co/rest/v1/'
+export const SUPABASE_URL = 'https://enaickcisttilfkiclyv.supabase.co'
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVuYWlja2Npc3R0aWxma2ljbHl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2Nzk4MjMsImV4cCI6MjA5NjI1NTgyM30.YTj69mAqG_6ysGaFmYLMMWoZ9QkjoLpGdXNKFIRh9Yk'
